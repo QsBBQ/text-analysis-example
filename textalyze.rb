04e4452e3c529be1b743fc6093ae40ec2d6af4a1
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-26 18:49:33 -0500

    Print item counts with nice formatting; completes v0.1
    format_counts() takes a hash of item/count pairs and returns a
    pleasantly-formatted string for printing.

--- textalyze.rb
+++ textalyze.rb
@@ -8,6 +8,12 @@ def item_counts(array)
   counts
 end
 
+def format_counts(counts)
+  counts.map do |item, count|
+    "#{item} - #{count}"
+  end.join("\n")
+end
+
 if ARGV[0] == "test"
   p item_counts([1,2,1,2,1]) == {1 => 3, 2 => 2}
   p item_counts(["a","b","a","b","a","ZZZ"]) == {"a" => 3, "b" => 2, "ZZZ" => 1}
@@ -15,4 +21,9 @@ if ARGV[0] == "test"
   p item_counts(["hi", "hi", "hi"]) == {"hi" => 3}
   p item_counts([true, nil, "dinosaur"]) == {true => 1, nil => 1, "dinosaur" => 1}
   p item_counts(["a","a","A","A"]) == {"a" => 2, "A" => 2}
+
+  sample_items = ["a", "a", "a", "b", "b", "c"]
+
+  puts "The counts for #{sample_items} are..."
+  puts format_counts( item_counts(sample_items) )
 end
