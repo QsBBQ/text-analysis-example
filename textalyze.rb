ee64c7279f7406b59f1e6dcfda26488be0cf5ea0
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-26 18:43:53 -0500

    Run tests conditionally w/ "test" argument
    Run tests by providing an argument "test":
    $ ruby textalyze.rb test

--- textalyze.rb
+++ textalyze.rb
@@ -8,9 +8,11 @@ def item_counts(array)
   counts
 end
 
-p item_counts([1,2,1,2,1]) == {1 => 3, 2 => 2}
-p item_counts(["a","b","a","b","a","ZZZ"]) == {"a" => 3, "b" => 2, "ZZZ" => 1}
-p item_counts([]) == {}
-p item_counts(["hi", "hi", "hi"]) == {"hi" => 3}
-p item_counts([true, nil, "dinosaur"]) == {true => 1, nil => 1, "dinosaur" => 1}
-p item_counts(["a","a","A","A"]) == {"a" => 2, "A" => 2}
+if ARGV[0] == "test"
+  p item_counts([1,2,1,2,1]) == {1 => 3, 2 => 2}
+  p item_counts(["a","b","a","b","a","ZZZ"]) == {"a" => 3, "b" => 2, "ZZZ" => 1}
+  p item_counts([]) == {}
+  p item_counts(["hi", "hi", "hi"]) == {"hi" => 3}
+  p item_counts([true, nil, "dinosaur"]) == {true => 1, nil => 1, "dinosaur" => 1}
+  p item_counts(["a","a","A","A"]) == {"a" => 2, "A" => 2}
+end
